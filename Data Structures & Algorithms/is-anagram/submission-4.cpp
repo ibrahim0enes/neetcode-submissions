e198021a30559e4eb595e7dcc3a02bbf55c48889
İbrahim Enes <ibrahimens53@gmail.com>
2026-04-19 08:52:48 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) { 
+
+        if (s.size() != t.size()) return false;
+
+        std::unordered_map<char, int> count;
+        
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for (auto const& [key, val] : count) { 
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
